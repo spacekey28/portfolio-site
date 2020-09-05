4589eea97b9c97512deb5440d2f9e82495388f22
Peter Son <peteson.me@gmail.com>
2020-09-06 08:15:49 +1200

    work card hover effect update

--- src/components/work-card.js
+++ src/components/work-card.js
@@ -1,31 +1,37 @@
 import React from "react"
 
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import Card from "react-bootstrap/Card"
+import ListGroup from "react-bootstrap/ListGroup"
+import ListGroupItem from "react-bootstrap/ListGroupItem"
 
 export default function WorkCard(props) {
   return (
     <Card className="work-card">
-      <Card.Link className="work-card__thumb" href={props.linkUrl} target="_blank">
-        <Card.Img className="work-card__thumb-image" variant="top" src={props.imageUrl} />
+      <Card.Link
+        className="work-card__thumb"
+        href={props.linkUrl}
+        target="_blank"
+      >
+        <Card.Img
+          className="work-card__thumb-image"
+          variant="top"
+          src={props.imageUrl}
+        />
       </Card.Link>
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-          {props.bodyText}
-        </Card.Text>
+        <Card.Text>{props.bodyText}</Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
         <ListGroupItem>{props.toolset}</ListGroupItem>
       </ListGroup>
-      {
-        props.linkUrl && (
-          <Card.Body>
-            <Card.Link href={props.linkUrl} target="_blank">Visit Website</Card.Link>
-          </Card.Body>
-        )
-      }
+      {props.linkUrl && (
+        <Card.Body>
+          <Card.Link href={props.linkUrl} target="_blank">
+            Visit Website
+          </Card.Link>
+        </Card.Body>
+      )}
     </Card>
   )
 }
